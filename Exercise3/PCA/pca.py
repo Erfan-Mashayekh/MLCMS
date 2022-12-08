1e3f0889d1f656ec805e7e8bfde3825b84fc9dfb
lbr1998 <86569817+lbr1998@users.noreply.github.com>
2022-12-08 10:16:45 +0100

    update some representation

--- Exercise3/PCA/pca.py
+++ Exercise3/PCA/pca.py
@@ -24,12 +24,14 @@ def pca(data):
     #decompose the data through SVD
     U, s, Vh = np.linalg.svd(data_centered)
 
-    # construct the S matrix
+    #construct the S matrix
     s_square = np.square(s)
     trace = np.sum(s_square)
     S = np.zeros(shape=(data_centered.shape[0], data_centered.shape[1]))
     s = np.diag(s)
     S[:s.shape[0], :s.shape[1]] = s
+
+    #compute the energy for each principal component
     energy = s_square/trace
 
     return U, S, Vh, energy
